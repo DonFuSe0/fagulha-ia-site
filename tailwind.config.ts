bc2eea206d80666a2a9e4c3a6c344f39caeb8dba
DonFuSe <marlongpds@gmail.com>
2025-09-22 17:32:58 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,7 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        // Podemos adicionar nossas cores personalizadas aqui depois
-        primary: '#9333ea', // Roxo
-      }
-    },
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: { colors: { primary: '#9333ea' } } },
   plugins: [],
 };
 export default config;
