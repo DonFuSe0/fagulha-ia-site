e98237f4937de88427c51b6b2c72c843322ad41b
DonFuSe <marlongpds@gmail.com>
2025-09-22 17:34:14 -0300

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,5 @@
 import { createBrowserClient } from '@supabase/ssr';
-
 export function createClient() {
-  // As variáveis de ambiente que configuramos na Vercel serão usadas aqui
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
