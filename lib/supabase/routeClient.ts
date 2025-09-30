47aeba2aac54cf7c780c1bb265f242d01a05cacf
DonFuSe <marlongpds@gmail.com>
2025-09-30 19:22:23 -0300

    Update routeClient.ts

--- lib/supabase/routeClient.ts
+++ lib/supabase/routeClient.ts
@@ -2,7 +2,5 @@ import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
 export function supabaseRoute() {
-  return createRouteHandlerClient(
-    { cookies }
-  );
+  return createRouteHandlerClient({ cookies });
 }
