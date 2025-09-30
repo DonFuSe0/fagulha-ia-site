fbc19a6f9b10c15e016e6a970b88c95ddfc89dd9
DonFuSe <marlongpds@gmail.com>
2025-09-30 19:21:11 -0300

    Update serverClient.ts

--- lib/supabase/serverClient.ts
+++ lib/supabase/serverClient.ts
@@ -1,11 +1,6 @@
 import { cookies } from 'next/headers';
-import { createServerClient } from '@supabase/auth-helpers-nextjs';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export function supabaseServer() {
-  const cookieStore = cookies();
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: () => cookieStore }
-  );
+  return createServerComponentClient({ cookies });
 }
