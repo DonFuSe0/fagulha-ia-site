c69e648a61b246218c4b36e68e2ae8667f3c216d
DonFuSe <marlongpds@gmail.com>
2025-09-30 18:34:22 -0300

    Update serverClient.ts

--- lib/supabase/serverClient.ts
+++ lib/supabase/serverClient.ts
@@ -1,15 +1,11 @@
 import { cookies } from 'next/headers';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import type { Database } from '@/types/supabase';
+import { createServerClient } from '@supabase/auth-helpers-nextjs';
 
-/**
- * Returns a Supabase client for use inside Server Components. The client
- * automatically forwards cookies to Supabase to retrieve the current auth
- * session.
- */
 export function supabaseServer() {
   const cookieStore = cookies();
-  return createServerComponentClient<Database>({
-    cookies: () => cookieStore,
-  });
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: () => cookieStore }
+  );
 }
